7bb9bd5aa5a4bcf6f1693fc83920c640d3339433
savanahatieno <savanaht01@gmail.com>
2021-04-04 12:40:54 +0300

    generated search-user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
