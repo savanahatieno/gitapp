3dd6eadf3d667fbc7f0fd60f6f3a6ae85173f619
savanahatieno <savanaht01@gmail.com>
2021-04-04 13:30:47 +0300

    created class for repomodel

--- src/app/repo-service.service.ts
+++ src/app/repo-service.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoServiceService {
+  _URL = 'https://api.github.com/users/';
+  token = '?access_token=3cbbddb991c02e7d5a2f0fd4224f9267a5153e5f';
+  constructor(public  http: HttpClient) {
+  }
 
-  constructor() { }
+  getRepo(searchTerm: string): Observable<any> {
+    return this.http.get(this._URL + searchTerm + '/repos?' + this.token);
+  }
 }
